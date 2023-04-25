09e67af507c7e387b516c5d16b2329311090a67f
Adhyan H <adhyanpatel@gmail.com>
2023-04-24 19:54:30 -0600

    make sure inline asm is correct

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@
 
 use cortex_m::peripheral::{syst::SystClkSource};
 use cortex_m_rt::{entry, exception};
+use core::mem::MaybeUninit;
 use panic_halt as _;
 use core::arch::asm; // use arm assembly
 
 
+
+static mut TASK_QUEUE: MaybeUninit<TaskQueue> = MaybeUninit::uninit();
+static mut REGISTERS: [u32; 8] = [0; 8];
 
 struct TaskQueue
     {
@@ -28,16 +32,38 @@ impl TaskQueue
 
 fn switch_rtos_context()
     {
-    // swtich to the next task
-    unimplemented!();
-    unsafe  
+    let tq = unsafe { &mut TASK_QUEUE.assume_init_mut() };
+    let current_task = tq.current_task;
+    let next_task = if current_task == 0 { 1 } else { 0 };
+
+    // Save the current task's context
+    unsafe 
         {
         asm!(
             "mrs r0, psp",
-            "stmdb r0!, {{r4-r11}}",
-            "msr psp, $0",
-            )
+            "stmia r0!, {{r4-r11}}", // save r4-r11 to stack
+            "mov r1, $0" : : "r" (&mut REGISTERS as *mut [u32; 8]) : "r1" : "volatile", "intel",
+            );
+        REGISTERS.copy_from_slice(&[current_task as u32]);
         }
+
+    // Switch to the next task
+    tq.current_task = next_task;
+
+    // Restore the next task's context
+    let next_task_registers = unsafe { &REGISTERS[(next_task * 8)..(next_task * 8 + 8)] };
+
+    unsafe 
+        {
+        asm!(
+            "mov r1, $0" :: "r" (next_task_registers as *const [u32; 8]) : "r1" : "volatile", "intel",
+            "ldmia r0!, {{r4-r11}}", // restore r4-r11 from stack
+            "msr psp, r0",
+            "bx lr",
+            options(noreturn)
+            );
+        }
+
     }
 
 #[exception]
@@ -70,15 +96,11 @@ fn setup() -> !
     syst.set_clock_source(SystClkSource::Core);
     setup_systick(&mut syst, interrupt_clock_cycles);
 
-    let task_queue = TaskQueue::new();
-    // main RTOS loop
-    loop
+    unsafe
         {
-        for task in task_queue.tasks.iter()
-            {
-            task();
-            }
-        }
+        let tq = MaybeUninit::new(TaskQueue::new());
+        tq.assume_init().tasks[0]();
+        };
     }
 
 #[no_mangle]
